d3c174fabf3eb88d4f1ddef26cef2f6cfecd4b02
sarbjeet <sarbjeetb2@gmail.com>
2023-03-24 10:52:39 -0600

    3 question completed

---
+++ Labskeleton.sql
@@ -0,0 +1,38 @@
+rem Lab Unit 10-11 Simple SELECT and Sorting 
+set echo on
+set linesize 132
+set pagesize 66
+spool c:\cprg250s\unit10-11LabOutput.txt
+
+rem Q1 - basic select columns with headers
+
+select first_name as "FIRST_NAME",last_name as"LAST_NAME" 
+from rcv_agent
+WHERE agent_speciality in('US','EU') and  agent_level in('III' ,'IV') 
+order by 2
+;
+
+select tour_description as"TOUR_DESCRIPTION"
+FROM rcv_vacation_tour
+WHERE rating_code in('E','M') AND tour_description like '%Paris%' 
+order by 1;
+
+rem Q3 
+column "COUNTRY" format A30
+select country as"COUNTRY",dest_description as "DEST_DESCRIPTION"
+FROM rcv_destination
+WHERE country in('Canada','United States')
+order by 1,2;
+clear columns
+
+
+rem Q4 
+
+
+
+rem Q5 
+
+rem insert solution here!
+
+spool off
+
